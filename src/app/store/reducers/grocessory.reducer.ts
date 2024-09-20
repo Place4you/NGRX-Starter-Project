f8ac3b6ed09b947414718de9180fde42ca9990e5
Place4you <sadiraja500@gmail.com>
2024-09-20 19:49:24 +0500

    reducers

---
+++ src/app/store/reducers/grocessory.reducer.ts
@@ -0,0 +1,6 @@
+const initalState =[
+    { "id": 1, "name": "mango","type":"fruit"},
+    { "id": 2, "name": "Ghobi","type":"vegetable"},
+    { "id": 3, "name": "bhinid","type":"vegetable"},
+    { "id": 4, "name": "apple","type":"fruit"},
+]
