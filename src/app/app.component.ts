62cb83b4f1ef5b5c820e9b3c05df55b2b131e93e
Place4you <sadiraja500@gmail.com>
2024-09-29 17:37:10 +0500

    LAYOUT UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BucketComponent } from './components/bucket/bucket.component';
 import { GroceryComponent } from './components/grocery/grocery.component';
+import { HeadingComponent } from "./components/heading/heading.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BucketComponent,GroceryComponent],
+  imports: [RouterOutlet, BucketComponent, GroceryComponent, HeadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
